a041f3c221b415755f2dc827ad7456220f3411ef
WilliamKinaan <w.kinaan@gmail.com>
2015-10-26 23:53:19 +0000

    addBulkBuilding

--- src/com/elasticsearch/bulkinsert/constants/IConstants.java
+++ src/com/elasticsearch/bulkinsert/constants/IConstants.java
@@ -3,7 +3,13 @@ package com.elasticsearch.bulkinsert.constants;
 public final class IConstants {
 	public static final String serverIpOrName = "localhost";
 	public static final int serverPort = 9200;
-	public static final String indexName = "william";
-	public static final String typeName = "william";
+	public static final String indexName = "william2";
+	public static final String typeName = "william2";
 	public static final String httpMethodPut = "PUT";
+	public static final String building = "building";
+	public static final String emirate = "emirate";
+	public static final String country = "country";
+	public static final String address = "address";
+	public static final String latitude = "latitude";
+	public static final String longitude = "longitude";
 }
--- src/com/elasticsearch/bulkinsert/webservices/Bulk.java
+++ src/com/elasticsearch/bulkinsert/webservices/Bulk.java
@@ -1,13 +1,18 @@
 package com.elasticsearch.bulkinsert.webservices;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -15,6 +20,8 @@ import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.elasticsearch.bulkinsert.constants.IConstants;
 import com.elasticsearch.helper.Util;
@@ -38,5 +45,74 @@ public class Bulk {
 		String prettyResponse = Util.uglyJsonToPrettyJson(responseString);
 		System.out.println(prettyResponse);
 		return Response.ok(prettyResponse, MediaType.APPLICATION_JSON).build();
+	}
+
+	@GET
+	@Path("/insertFromFile")
+	public Response insertFromFile(@QueryParam("filePath") String filePath) {
+
+		try {
+			ClassLoader classLoader = new Bulk().getClass().getClassLoader();
+			String newFilePath = classLoader.getResource(filePath).getFile();
+			File file = new File(newFilePath);
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferReader = new BufferedReader(fileReader);
+			StringBuilder builder = new StringBuilder();
+			String line = null;
+			while ((line = bufferReader.readLine()) != null) {
+				builder.append(line);
+			}
+			bufferReader.close();
+			JSONArray array = new JSONArray(builder.toString());
+			StringBuilder bulkRequestBody = new StringBuilder();
+			for (int i = 0; i < array.length(); i++) {
+				JSONObject oneJSON = array.getJSONObject(i);
+				String building = oneJSON.getString(IConstants.building);
+				String emirate = oneJSON.getString(IConstants.emirate);
+				String country = oneJSON.getString(IConstants.country);
+				String address = oneJSON.getString(IConstants.address);
+				String latitude = oneJSON.getDouble(IConstants.latitude) + "";
+				String longitude = oneJSON.getDouble(IConstants.longitude) + "";
+				bulkRequestBody.append("{");
+				bulkRequestBody.append("\"create\":{}");
+				bulkRequestBody.append("}\n");
+
+				bulkRequestBody.append("{");
+				bulkRequestBody.append("\"building\":\"").append(building)
+						.append("\",");
+				bulkRequestBody.append("\"emirate\":\"").append(emirate)
+						.append("\",");
+				bulkRequestBody.append("\"country\":\"").append(country)
+						.append("\",");
+				bulkRequestBody.append("\"address\":\"").append(address)
+						.append("\",");
+				bulkRequestBody.append("\"latitude\":\"").append(latitude)
+						.append("\",");
+				bulkRequestBody.append("\"longitude\":\"").append(longitude)
+						.append("\"");
+				bulkRequestBody.append("}\n");
+			}
+			HttpHost host = new HttpHost(IConstants.serverIpOrName,
+					IConstants.serverPort);
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpPut request = new HttpPut("/" + IConstants.indexName + "/"
+					+ IConstants.typeName + "/_bulk");
+			StringEntity body = new StringEntity(bulkRequestBody.toString());
+			request.setEntity(body);
+			HttpResponse response = client.execute(host, request);
+			String responseString = new BasicResponseHandler()
+					.handleResponse(response);
+			String prettyResponse = Util.uglyJsonToPrettyJson(responseString);
+			System.out.println(prettyResponse);
+			return Response.ok(prettyResponse, MediaType.APPLICATION_JSON)
+					.build();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return Response.status(200).entity("fuck god file doesn't exist")
+					.build();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return Response.status(500).build();
+		}
 	}
 }
